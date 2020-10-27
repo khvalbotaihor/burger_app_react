590464c8547f5d0309a428ba9329f40ad34f120f
Ihor Khvalbota Software Engineer <ihorkhvalbota@gmail.com>
2020-10-27 10:41:40 +0200

    - add ordersummary component

---
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Aux from '../../../hoc/Auxilary'
+
+const orderSummary = (props) =>{
+return(
+    <Aux>
+
+        <h3>Your Order</h3>
+        <p>A delicious burger with the folliwing ingredients:</p>
+    </Aux>
+)
+}
+
+export default orderSummary;
