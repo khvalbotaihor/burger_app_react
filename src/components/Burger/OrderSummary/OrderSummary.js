09b5d6ed405d855488f0bd767ba2617c86db6ec2
Ihor Khvalbota Software Engineer <ihorkhvalbota@gmail.com>
2020-10-27 10:47:35 +0200

    - added ingredientSummary to OrderSummary component

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -18,8 +18,9 @@ return(
         <h3>Your Order</h3>
         <p>A delicious burger with the folliwing ingredients:</p>
         <ul>
-
+            {ingredientSummary}
         </ul>
+        <p>Continue to Checkout?</p>
     </Aux>
 )
 }
