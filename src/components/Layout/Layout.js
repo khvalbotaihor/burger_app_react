ce8f68bf71001729aaac195ec95fde2a5581b193
Ihor Khvalbota Software Engineer <ihorkhvalbota@gmail.com>
2020-10-27 15:02:36 +0200

    - create new sideDrawerCloseHandler

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,17 +1,29 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Aux from '../../hoc/Auxilary';
 import classes from './Layout.css'
 import Toolbar from '../Navigation/Toolbar/Toolbar';
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer'
 
-const layout = (props) => (
-    <Aux>
-        <Toolbar />
-        <SideDrawer />
-        <main className={classes.Content}>
-            {props.children}
-        </main>
-    </Aux>
-);
+class Layout extends Component {
 
-export default layout;
+    state={
+        showSideDrawer: true
+    }
+
+    sideDrawerCloseHandler = () =>{
+        this.setState({showSideDrawer: false})
+    }
+
+
+    render() {
+        return <Aux>
+            <Toolbar/>
+            <SideDrawer closed={this.sideDrawerCloseHandler}/>
+            <main className={classes.Content}>
+                {this.props.children}
+            </main>
+        </Aux>
+    }
+}
+
+export default Layout;
