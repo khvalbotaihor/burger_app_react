5f5ef0ee4ec9486ea4f40549d34f11948c40565a
Ihor Khvalbota Software Engineer <ihorkhvalbota@gmail.com>
2020-10-27 13:22:47 +0200

    - added navigation items

--- src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
+++ src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
@@ -4,9 +4,9 @@ import classes from './NavigationItem.css'
 const navigationItem = (props) =>(
     <li className={classes.NavigationItem}>
         <a
-            href{props.link}
+            href={props.link}
             className={props.active ? classes.active : null}
-        ></a>{props.children}</li>
+        >{props.children}</a></li>
 
 )
 
