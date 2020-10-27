1a3f01702d71335749bb77f80ea970134272da9b
Ihor Khvalbota Software Engineer <ihorkhvalbota@gmail.com>
2020-10-27 16:09:58 +0200

    - change files structure

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react'
-import Aux from '../../hoc/Auxilary';
+import Aux from '../../hoc/Auxiliary/Auxilary';
 import Burger from '../../components/Burger/Burger'
 import BuildControls from '../../components/Burger/BuilControls/BuildControls'
 import Modal from '../../components/UI/Modal/Modal';
