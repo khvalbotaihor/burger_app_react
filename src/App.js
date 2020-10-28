de593aadba0fe4a4d7bc8cd200267a79bdd6c700
Ihor Khvalbota Software Engineer <ihorkhvalbota@gmail.com>
2020-10-28 10:35:38 +0200

    - fixed componentdidmpont incorrect representing

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import Layout from './hoc/Layout/Layout'
 import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder";
 
 class App extends Component {
-  render() {
-    return (
-      <div >
+    render() {
+        return (
+            <div>
 
-        <Layout>
-            <BurgerBuilder></BurgerBuilder>
+                <Layout>
+                    {this.state.show ? <BurgerBuilder/> : null}
 
-        </Layout>
+                </Layout>
 
 
-
-      </div>
-    );
-  }
+            </div>
+        );
+    }
 }
 
 export default App;
