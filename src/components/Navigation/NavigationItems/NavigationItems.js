b598e5540473e1a47e0573ac0e9953a7dfdaccac
Ihor Khvalbota Software Engineer <ihorkhvalbota@gmail.com>
2020-10-29 16:16:22 +0200

    - added links

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -1,18 +1,13 @@
 import React from 'react';
 import classes from './NavigationItems.css';
 import NavigationItem from './NavigationItem/NavigationItem'
-import {Link} from "react-router-dom";
 
 
 const navigationItems = () => (
     <ul className={classes.NavigationItems}>
 
-        <Link to='/'>
-        <NavigationItem link="/" active>Burger Builder</NavigationItem>
-        </Link>
-        <Link to='/checkout'>
-        <NavigationItem link="/">Checkout</NavigationItem>
-        </Link>
+        <NavigationItem link="/" exact>Burger Builder</NavigationItem>
+        <NavigationItem link="/orders">Orders</NavigationItem>
     </ul>
 )
 
