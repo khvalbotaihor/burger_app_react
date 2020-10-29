a21c27a8e6f4f823435963b88cb12a9985fcd611
Ihor Khvalbota Software Engineer <ihorkhvalbota@gmail.com>
2020-10-29 13:14:04 +0200

    - add route

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import classes from './NavigationItems.css';
 import NavigationItem from './NavigationItem/NavigationItem'
+import {Link} from "react-router-dom";
 
 
 const navigationItems = () => (
     <ul className={classes.NavigationItems}>
+
+        <Link to='/'>
         <NavigationItem link="/" active>Burger Builder</NavigationItem>
+        </Link>
+        <Link to='/checkout'>
         <NavigationItem link="/">Checkout</NavigationItem>
+        </Link>
     </ul>
 )
 
