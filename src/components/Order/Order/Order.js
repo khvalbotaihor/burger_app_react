5b5debee131eda7b1dd07083c5919f669bbb33fe
Ihor Khvalbota Software Engineer <ihorkhvalbota@gmail.com>
2020-10-29 16:50:05 +0200

    - add styles to orders page

--- src/components/Order/Order/Order.js
+++ src/components/Order/Order/Order.js
@@ -2,11 +2,38 @@ import React from 'react';
 import classes from './Order.css';
 
 
-const order = (props) =>(
-    <div className={classes.order}>
-        <p>Ingredients: Salad (1)</p>
-        <p>Price: <strong>USD {Number.parseFloat(props.price).toFixed(2)}</strong></p>
-    </div>
-)
+const order = (props) => {
+
+    const ingredients = [];
+
+    for (let ingredientName in props.ingredients) {
+        ingredients.push({
+            name: ingredientName,
+            amount: props.ingredients[ingredientName]
+        })
+
+    }
+
+    const ingredientsOutput = ingredients.map(ig => {
+        return <span
+            style={{
+                textTransformed:'capitalize',
+                display: 'inline-block',
+                margin: '0 8px',
+                border: '1px solid #ccc',
+                padding :'5px'
+
+            }}
+            key={ig.name}>{ig.name} ({ig.amount})</span>
+    })
+
+    return (
+        <div className={classes.order}>
+            <p>Ingredients: {ingredientsOutput}</p>
+            <p>Price: <strong>USD {Number.parseFloat(props.price).toFixed(2)}</strong></p>
+        </div>
+    )
+}
+
 
 export default order;
