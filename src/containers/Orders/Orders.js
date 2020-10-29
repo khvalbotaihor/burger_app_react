70ba18ceef517cf779665825e15ecdc1a4b0922d
Ihor Khvalbota Software Engineer <ihorkhvalbota@gmail.com>
2020-10-29 16:27:15 +0200

    - added componentdidmount

--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -1,16 +1,41 @@
 import React, {Component} from 'react';
 import Order from '../../components/Order/Order/Order';
-
+import axios from '../../axios-orders';
+import withErrorHandler from "../../hoc/withErrorHandler/withErrorHandler";
 
 class Orders extends Component {
+    state = {
+        orders: [],
+        loading: true
+    }
+
+
+    componentDidMount() {
+        axios.get('orders.json')
+            .then(res => {
+                const fetchOrders = [];
+
+                for(let key in res.data){
+                    fetchOrders.push({
+                        ...res.data[key],
+                        id:key
+                    })
+                }
+                this.setState({loading: false, orders: fetchOrders})
+            }).catch(error => {
+            this.setState({loading: false})
+
+        })
+    }
+
     render() {
         return (
             <div>
-                <Order />
-                <Order />
+                <Order/>
+                <Order/>
             </div>
         )
     }
 }
 
-export default Orders;
+export default withErrorHandler(Orders, axios);
