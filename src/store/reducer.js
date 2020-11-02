f649144d486e16bc40b89c1c5839c4c85cf9daf5
Ihor Khvalbota Software Engineer <ihorkhvalbota@gmail.com>
2020-11-02 06:58:19 +0200

    - add reducers data

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,7 +1,12 @@
 import * as ActionTypes from './actions';
 
 const initialState = {
-    ingredients: null,
+    ingredients: {
+        salad: 0,
+        bacon: 0,
+        cheese: 0,
+        meat: 0
+    },
     totalPrice: 4,
 }
 
@@ -9,12 +14,19 @@ const reducer = (state = initialState, action) => {
     switch (action.type) {
         case ActionTypes.ADD_INGREDIENT:
             return {
-
+                ...state,
+                ingredients: {
+                    ...state.ingredients,
+                    [action.ingredientName]: state.ingredients[action.ingredientName] + 1
+                }
             }
         case ActionTypes.REMOVE_INGREDIENT:
             return {
-
-            }
+                ...state,
+                ingredients: {
+                    ...state.ingredients,
+                    [action.ingredientName]: state.ingredients[action.ingredientName] -1
+                }}
         default:
             return state;
     }
