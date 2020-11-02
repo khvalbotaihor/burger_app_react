e6d059cfc8f1d9ae830e9e8af6bd0b5128dae492
Ihor Khvalbota Software Engineer <ihorkhvalbota@gmail.com>
2020-11-02 06:42:46 +0200

    - added additional logic to reducer component

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -5,8 +5,19 @@ const initialState = {
     totalPrice: 4,
 }
 
-const reducer = (state=initialState, action) =>{
+const reducer = (state = initialState, action) => {
+    switch (action.type) {
+        case ActionTypes.ADD_INGREDIENT:
+            return {
 
+            }
+        case ActionTypes.REMOVE_INGREDIENT:
+            return {
+
+            }
+        default:
+            return state;
+    }
 }
 
 export default reducer;
