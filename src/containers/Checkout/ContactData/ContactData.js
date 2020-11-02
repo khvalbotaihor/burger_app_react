e7f646f42008555d9ee15c3fd11b3909cbebe1f9
Ihor Khvalbota Software Engineer <ihorkhvalbota@gmail.com>
2020-11-02 07:52:59 +0200

    - add redux to contactdata component

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -4,6 +4,7 @@ import classes from './ContactData.css';
 import axios from '../../../axios-orders';
 import Spinner from '../../../components/UI/Spinner/Spinner';
 import Input from '../../../components/UI/Input/Input';
+import {connect} from 'react-redux'
 
 
 class ContactData extends Component {
@@ -86,8 +87,8 @@ class ContactData extends Component {
                         ],
                     },
                     value: '',
-                    validation:{},
-                    valid:true
+                    validation: {},
+                    valid: true
                 },
         },
         formIsValid: false,
@@ -97,7 +98,7 @@ class ContactData extends Component {
     checkValidity(value, rules) {
         let isValid = true;
 
-        if (!rules){
+        if (!rules) {
             return true;
         }
 
@@ -227,4 +228,12 @@ class ContactData extends Component {
     }
 }
 
-export default ContactData;
+const mapStateToProps = state => {
+    return {
+        ings: state.ingredients,
+        price: state.price
+
+    }
+}
+
+export default connect(mapStateToProps)(ContactData);
