16f87a3936261a42bdc47943b7647186d0d94526
Ihor Khvalbota Software Engineer <ihorkhvalbota@gmail.com>
2020-11-03 06:00:34 +0200

    - added redux devtools

--- src/index.js
+++ src/index.js
@@ -8,7 +8,8 @@ import {Provider} from 'react-redux';
 import {createStore} from "redux";
 import reducer from "./store/reducer";
 
-const store = createStore(reducer);
+const argument = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+const store = createStore(reducer,argument);
 
 const app = (
     <Provider store={store}>
