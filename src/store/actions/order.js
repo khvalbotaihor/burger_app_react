894cb15434210671a30d1e563c0268fb6f2ff17d
Ihor Khvalbota Software Engineer <ihorkhvalbota@gmail.com>
2020-11-04 05:29:55 +0200

    - add new actions creators

--- src/store/actions/order.js
+++ src/store/actions/order.js
@@ -37,8 +37,48 @@ export const purchaseBurger = (orderData) => {
     }
 }
 
-export const purchaseInit = () =>{
+export const purchaseInit = () => {
     return {
         type: actionTypes.PURCHASE_INIT
+    }
+}
+
+export const fetchOrdersSuccess = (orders) => {
+    return {
+        type: actionTypes.FETCH_ORDERS_SUCCESS,
+        orders: orders
+    }
+}
+export const fetchOrdersFail = (error) => {
+    return {
+        type: actionTypes.FETCH_ORDERS_FAIL,
+        error: error
+    }
+}
+export const fetchOrdersStart = () => {
+    return {
+        type: actionTypes.FETCH_ORDERS_START
+    }
+}
+
+export const fetchOder = () => {
+    return dispatch => {
+        axios.get('/order.json')
+            .then(res => {
+                const fetchOrders = [];
+                console.log('result:');
+                console.log(res);
+                for (let key in res.data) {
+                    fetchOrders.push({
+                        ...res.data[key],
+                        id: key
+                    })
+                }
+                dispatch(fetchOrdersSuccess(fetchOrders));
+                this.setState({loading: false, orders: fetchOrders})
+            }).catch(error => {
+            this.setState({loading: false})
+
+        })
     }
 }
