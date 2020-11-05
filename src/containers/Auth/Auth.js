19613ee331d91b19e097329cc854cbacdc3b4a9c
Ihor Khvalbota Software Engineer <ihorkhvalbota@gmail.com>
2020-11-05 05:59:19 +0200

    - added event handling for auth form

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -2,6 +2,8 @@ import React, {Component} from 'react';
 import Input from '../../components/UI/Input/Input';
 import Button from '../../components/UI/Button/Button';
 import classes from './Auth.css';
+import * as actions from '../../store/actions/index';
+import {connect} from 'react-redux';
 
 
 class Auth extends Component {
@@ -73,6 +75,11 @@ class Auth extends Component {
         this.setState({controls: updatedControls})
     }
 
+    submitHandler = (event) => {
+        event.preventDefault();
+        this.props.onAuth(this.state.controls.email.value, this.state.controls.password.value)
+
+    }
 
 
 
@@ -101,7 +108,7 @@ class Auth extends Component {
 
         return (
             <div className={classes.Auth}>
-                <form>
+                <form onSubmit={this.submitHandler}>
                     {form}
                     <Button btnType='Success'>Submit</Button>
 
@@ -111,4 +118,11 @@ class Auth extends Component {
     }
 }
 
-export default Auth;
+const mapDispatchToProps = dispatch => {
+    return{
+        onAuth: (email, password) => dispatch(actions.auth(email, password))
+    }
+}
+
+
+export default connect(null, mapDispatchToProps)(Auth);
