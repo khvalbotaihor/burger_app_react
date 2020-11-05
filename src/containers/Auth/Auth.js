20d211360f2d4a3dbf19b47a2400c047fe5ac1bc
Ihor Khvalbota Software Engineer <ihorkhvalbota@gmail.com>
2020-11-05 04:40:49 +0200

    - created auth component

---
+++ src/containers/Auth/Auth.js
@@ -0,0 +1,48 @@
+import React, {Component} from 'react';
+
+class App extends Component{
+
+    state={
+        controls: {
+            email: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'email',
+                    placeholder: 'Mail address'
+                },
+                value: '',
+                validation: {
+                    required: true,
+                    isEmail: true
+                },
+                valid: false,
+                touched: false
+            },
+            password: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'password',
+                    placeholder: 'Mail address'
+                },
+                value: '',
+                validation: {
+                    required: true,
+                    minLength: 6
+                },
+                valid: false,
+                touched: false
+            }
+        },
+    }
+
+    render() {
+        return (
+            <div>
+                <form>
+
+
+                </form>
+            </div>
+        )
+    }
+}
