308696e615237e3d511abb54611ab491d939d84e
Ihor Khvalbota Software Engineer <ihorkhvalbota@gmail.com>
2020-11-05 06:33:06 +0200

    - added auth action creator

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -1,3 +1,5 @@
+import axios from 'axios';
+
 import * as actionTypes from '../actions/actionTypes';
 
 export const authStart = () =>{
@@ -22,6 +24,22 @@ export const auth = (email, password) =>{
     return dispatch => {
         // ... authenticate user
         dispatch(authStart());
+
+        const authData = {
+            email: email,
+            password: password,
+            returnSecureToken: true
+        }
+
+        axios.post('https://identitytoolkit.googleapis.com/v1/accounts:signInWithCustomToken?key=AIzaSyBmSFTDYjbvZYlS-WAM1SSqBnRUabG23m0', authData)
+            .then(response =>{
+                console.log(response);
+                dispatch(authSucess(response.data))
+            })
+            .catch(err => {
+                console.log(err);
+                dispatch(authFail())
+            })
 
     }
 }
